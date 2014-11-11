07a8fd655cd0f11b7697f5e3aca7460b8f1ef27b
Kasia Ryniak <kasiaryniak@Kasias-MacBook-Air.local>
2014-11-11 16:34:33 +0100

    reszta plikow

---
+++ hello.py
@@ -0,0 +1,16 @@
+#def hi(imie):
+#	if imie == 'Aga':
+#		print('Cześć Aga!')
+#	elif imie == 'Arleta':
+#		print('Cześć Arleta!')
+#	else:
+#		print ('Cześć!')
+#hi('Aga')
+
+names = ['Aga', 'Arleta', 'Kasia']
+for name in names:
+	print("Hello "+name)
+
+
+
+
